2b36d969ab60b52cf8d5aaae94229bc14b1042ad
CreeperSan <admin@creepersan.com>
2020-03-16 23:32:02 +0800

    [Add] add overwatch-league-logo

--- routes/display.js
+++ routes/display.js
@@ -6,5 +6,8 @@ router.get('/hex_clock', async (ctx, next) => {
     await ctx.render('display/hex_clock')
 })
 
+router.get('/overwatch-league-logo', async (ctx, next) => {
+    await ctx.render('display/overwatch-league-logo')
+})
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ router.get('/', async (ctx, next) => {
               description : '如果把当前时间作为RGB颜色...',
               background_url : '/res/index/img_thumbnail_display_hex_clock.jpg',
               url : '/display/hex_clock'
+            },{
+              title : '守望先锋战队Logo',
+              description : '只是守望先锋职业联赛战队的Logo...',
+              background_url : '/res/index/img_thumbnail_display_overwatch_league_logo.png',
+              url : '/display/overwatch-league-logo'
             }
           ]
         }
